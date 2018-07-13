19b65ca8e770ea0ae169ec37fa6e8855a665adf7
hamoudimolka <41198636+hamoudimolka@users.noreply.github.com>
2018-07-13 16:07:32 +0100

    Add files via upload

---
+++ js/main .js
@@ -0,0 +1,16 @@
+alert("Molka is the best head of leads ever");
+var a = 4 ;
+var b = 3 ; 
+function sum(a,b) 
+{
+    return a+b;
+}
+var a = sum(3,4); 
+	var b = sum(a,a); 
+    var c = sum(a,b); 
+    var a =prompt ("what's your name?");
+    alert(a);
+    confirm ('can i have your name?');
+    setTimeout (aquarium , 3000);
+
+    
